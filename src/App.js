066fe24293c7a57468d9fa2cbe0c575de56330b4
HamdiTliba <hamditliba.go@gmail.com>
2023-05-09 18:51:33 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import datalist from "./components/datalist";
+import summary from "./components/summary";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  // const [shows, setShows] = useState([]);
+  // const [data, setData] = useState([]);
+  const url = "https://api.tvmaze.com/search/shows?q=all";
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        // setData(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/shows/:id" element={<datalist />} />
+        <Route path="/" element={<summary />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/datalist.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const datalist = () => {
+  return <div>data</div>;
+};
+
+export default datalist;
